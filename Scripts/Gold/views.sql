80dc26ad7fd25e4c86973b284d3241cde7db7655
pavanaddanki152 <pavanaddanki944@gmail.com>
2026-02-09 12:02:23 +0530

    Refactor SQL views for better clarity and structure
    Refactored views for gold.dim_customers, gold.dim_products, and gold.fact_sales to improve structure and readability.

--- Scripts/Gold/views.sql
+++ Scripts/Gold/views.sql
@@ -1,36 +1,30 @@
 
-IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
-    DROP VIEW gold.dim_customers;
-GO
-
-CREATE VIEW gold.dim_customers AS
+CREATE VIEW gold.dim_customers as 
 SELECT
-    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
+ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
     ci.cst_id                          AS customer_id,
     ci.cst_key                         AS customer_number,
     ci.cst_firstname                   AS first_name,
     ci.cst_lastname                    AS last_name,
-    la.cntry                           AS country,
-    ci.cst_marital_status              AS marital_status,
-    CASE 
+	CASE 
         WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the primary source for gender
         ELSE COALESCE(ca.gen, 'n/a')  			   -- Fallback to ERP data
     END                                AS gender,
-    ca.bdate                           AS birthdate,
-    ci.cst_create_date                 AS create_date
-FROM silver.crm_cust_info ci
-LEFT JOIN silver.erp_cust_az12 ca
+	ci.cst_create_date                 AS create_date,
+    ci.cst_marital_status              AS marital_status,
+	    ca.bdate                        AS birthdate,
+		la.cntry                           AS country
+	from silver.crm_cust_info ci
+	LEFT JOIN silver.erp_cust_az12 ca
     ON ci.cst_key = ca.cid
-LEFT JOIN silver.erp_loc_a101 la
+	LEFT JOIN silver.erp_loc_a101 la
     ON ci.cst_key = la.cid;
-GO
 
--- =============================================================================
--- Create Dimension: gold.dim_products
--- =============================================================================
-IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
-    DROP VIEW gold.dim_products;
-GO
+
+
+
+
+
 
 CREATE VIEW gold.dim_products AS
 SELECT
@@ -48,15 +42,13 @@ SELECT
 FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
     ON pn.cat_id = pc.id
-WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
-GO
+WHERE pn.prd_end_dt IS NULL;
 
--- =============================================================================
--- Create Fact Table: gold.fact_sales
--- =============================================================================
-IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
-    DROP VIEW gold.fact_sales;
-GO
+
+
+
+
+
 
 CREATE VIEW gold.fact_sales AS
 SELECT
@@ -74,4 +66,5 @@ LEFT JOIN gold.dim_products pr
     ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
     ON sd.sls_cust_id = cu.customer_id;
-GO
+
+    select * from gold.fact_sales
